904e2ea80e950ae3228473c2d7b51c6428ea305f
Suzy Hlinka <snh7@njit.edu>
2020-04-07 21:43:51 -0400

    get data

--- projectmanager/src/index.js
+++ projectmanager/src/index.js
@@ -1,17 +1,18 @@
+//import
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component{
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    getData(){
+        axios.get('http://my-json-server.typicode.com/bnissen24/project2DB/posts')
+            .then(response => {
+                this.setState({tasks: response.data});
+            }).catch (error => {
+                this.setState({errorMessage: error.message});
+        });
+    }
+}
+
+//render react component into DOM

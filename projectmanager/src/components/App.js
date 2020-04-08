1085ef387a030b9a5f8b119985aa0e70771df045
Suzy Hlinka <snh7@njit.edu>
2020-04-08 19:37:55 -0400

    moved code from index.js to app component

---
+++ projectmanager/src/components/App.js
@@ -0,0 +1,19 @@
+//import
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={};
+    }
+    getData(){
+        axios.get('http://my-json-server.typicode.com/bnissen24/project2DB/posts')
+            .then(response => {
+                this.setState({tasks: response.data});
+            }).catch (error => {
+            this.setState({errorMessage: error.message});
+        });
+    }
+}

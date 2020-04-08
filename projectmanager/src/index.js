e8685905cf4068fa99b778f2f462b5892cc37e92
Suzy Hlinka <snh7@njit.edu>
2020-04-07 22:43:21 -0400

    added constructor

--- projectmanager/src/index.js
+++ projectmanager/src/index.js
@@ -4,7 +4,10 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 class App extends React.Component{
-
+    constructor(props){
+        super(props);
+        this.state={};
+    }
     getData(){
         axios.get('http://my-json-server.typicode.com/bnissen24/project2DB/posts')
             .then(response => {

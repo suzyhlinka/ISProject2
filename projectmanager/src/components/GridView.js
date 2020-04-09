027d23f5fe570fc23ef5163698ce1537e6df2031
Suzy Hlinka <snh7@njit.edu>
2020-04-08 22:27:57 -0400

    added jsx for grid item in a grid view

--- projectmanager/src/components/GridView.js
+++ projectmanager/src/components/GridView.js
@@ -24,10 +24,78 @@ class GridView extends React.Component{
         if (column_pos > 0){
             column_pos -= 1;
             item.column = columns[column_pos];
+            ////
         }
     }
 
+    goNext(task_id, task_column){
+        let item = this.searchForItem(task_id, task_column);
+        let column_pos = columns.findIndex(name => item.column ===name);
+
+        if (column_pos < columns.length()){
+            column_pos += 1;
+            item.column = columns[column_pos];
+            ////
+        }
+    }
+
+    renderItemColumn(post, prev_text, next_text){
+        return (
+            <GridItem id = {post.id}
+                      key = {post.id}
+                      title = {post.title}
+                      type = {post.type}
+                      column = {post.column}
+                      prev_text = {prev_text}
+                      goBack = {this.goBack}
+                      next_text = {next_text}
+                      goNext = {this.goNext} />
+        );
+    }
+
     render(){
+        const todo_list = this.props.items.todo.map(post => this.renderItemColumn(post, "",'In Progress ->'));
+        const inProgress_list = this.props.items.todo.map(post => this.renderItemColumn(post, "<- Send back to In Progess",'Request for Review ->'));
+        const review_list = this.props.items.todo.map(post => this.renderItemColumn(post, "<- Needs Review",'Complete ->'));
+        const done_list = this.props.items.todo.map(post => this.renderItemColumn(post, "<- Not Done",""));
+
+        return (
+            <div className = "gridView">
+                <div className = "gridView-column gridView-column-todo">
+                    <h3 className = "gridView-column-title">
+                        To-Do
+                    </h3>
+                    <div className = "gridView-item-container">
+                        {todo_list}
+                    </div>
+                </div>
+                <div className = "gridView-column gridView-column-inProgress">
+                    <h3 className = "gridView-column-title">
+                        In Progress
+                    </h3>
+                    <div className = "gridView-item-container">
+                        {inProgress_list}
+                    </div>
+                </div>
+                <div className = "gridView-column gridView-column-review">
+                    <h3 className = "gridView-column-title">
+                        Under Review
+                    </h3>
+                    <div className = "gridView-item-container">
+                        {review_list}
+                    </div>
+                </div>
+                <div className = "gridView-column gridView-column-done">
+                    <h3 className = "gridView-column-title">
+                        Done
+                    </h3>
+                    <div className = "gridView-item-container">
+                        {done_list}
+                    </div>
+                </div>
+            </div>
+
+        );
 
     }
 }

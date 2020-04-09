24db504bb4b14b832f8ebd85ada37edd35ae243c
Suzy Hlinka <snh7@njit.edu>
2020-04-08 20:29:09 -0400

    updated app component. removed old stuff not needed

--- projectmanager/src/components/App.js
+++ projectmanager/src/components/App.js
@@ -2,20 +2,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import GridView from './GridView';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
+    state={
+        view: 'grid',
+        task_list: [],
+        sorted_tasks: {
+            todo_items: [],
+            inProgress_items: [],
+            review_items: [],
+            done_items: []
+        }
     }
-}
-getData(){
-    axios.get('http://my-json-server.typicode.com/bnissen24/project2DB/posts')
-        .then(response => {
-            this.setState({tasks: response.data});
-        }).catch (error => {
-        this.setState({errorMessage: error.message});
-    });
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData(){
+        axios.get('http://my-json-server.typicode.com/bnissen24/project2DB/posts')
+            .then(response => {
+                this.setState({tasks: response.data});
+            }).catch (error => {
+            this.setState({errorMessage: error.message});
+        });
+    }
 }
 
 export default App;

adefd9f9cb2aa0b440dc1e888a715e4872b07555
Suzy Hlinka <snh7@njit.edu>
2020-04-08 20:02:18 -0400

    fixed App.js app class and made grid item component

--- projectmanager/src/components/App.js
+++ projectmanager/src/components/App.js
@@ -3,17 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-class App extends React.Component{
-    constructor(props){
+class App extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={};
-    }
-    getData(){
-        axios.get('http://my-json-server.typicode.com/bnissen24/project2DB/posts')
-            .then(response => {
-                this.setState({tasks: response.data});
-            }).catch (error => {
-            this.setState({errorMessage: error.message});
-        });
+        this.state = {};
     }
 }
+getData(){
+    axios.get('http://my-json-server.typicode.com/bnissen24/project2DB/posts')
+        .then(response => {
+            this.setState({tasks: response.data});
+        }).catch (error => {
+        this.setState({errorMessage: error.message});
+    });
+}
+
+export default App;
---
+++ projectmanager/src/components/GridItem.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const renderButton
+
+export default GridItem;

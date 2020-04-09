940d628efd6fbf207d3d8fc5a3642b12a836a97b
Suzy Hlinka <snh7@njit.edu>
2020-04-08 20:16:09 -0400

    created the griditem component

--- projectmanager/src/components/GridItem.js
+++ projectmanager/src/components/GridItem.js
@@ -1,5 +1,43 @@
 import React from 'react';
 
-const renderButton
+const render_button = (task_id, talk_column, button_text, task_function) => {
+    if (button_text && task_function){
+        return(
+            <div>
+                <a href="#"
+                   className="X"
+                   onClick={onItemClick (task_id, task_column, task_function)}>
+                    {button_text}
+                </a>
+            </div>
+        );
+    }
+    else{
+        return <div />
+    }
+}
+
+const onItemClick = (task_id, task_column, task_function) =>{
+    return () => {
+        task_function(task_id, task_column);
+    };
+}
+
+const GridItem = props => {
+    return (
+        <div className="item">
+            <div className="item-body">
+                <h4 className="item-title">
+                    {props.title}
+                </h4>
+                <div className="item-text">
+                    <div>ID: {props.id}</div>
+                    <div>Type: {props.type}</div>
+                    {render_button(props.id, props.column, props.last_text, props.last_function)}
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default GridItem;

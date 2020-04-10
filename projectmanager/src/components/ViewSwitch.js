2d4df49ee72ab0d8be6b940997293b7b1f2d040f
Suzy Hlinka <snh7@njit.edu>
2020-04-09 21:12:44 -0400

    basic layout for view switcher

---
+++ projectmanager/src/components/ViewSwitch.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ViewSwitch extends React.Component{
+
+    render(){
+        return(
+            <div className = "navigation">
+                <ul className = "nav_list">
+                    <li className = "nav_item">
+                        <a className = ""> Grid View</a>
+                    </li>
+                    <li className = "nav_item">List View</li>
+                </ul>
+            </div>
+        )
+    }
+}
